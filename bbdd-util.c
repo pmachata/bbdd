bf8a6fe0cda88d593baa49baccc6f04dff8a1399
Petr Machata <petrm@nvidia.com>
2026-04-15 17:11:04 +0200

    bbdd-util: Add bbdd_util_wraperr(), bbdd_util_vfmterr()
    bbdd_util_wraperr() is like bbdd_util_fmterr(), but the passed-in message
    pointer itself can be used as an argument in the error message.
    
    bbdd_util_vfmterr() is va_list variant of bbdd_util_fmterr(). It's used as
    a helper internally by bbdd_util_wraperr() and bbdd_util_fmterr(), so why
    not publish it as well.
    
    Signed-off-by: Petr Machata <petrm@nvidia.com>

--- bbdd-util.c
+++ bbdd-util.c
@@ -3,24 +3,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int bbdd_util_vfmterr(char **strp, const char *fmt, va_list ap)
+{
+	int rc;
+
+	if (!strp)
+		return 0;
+
+	rc = vasprintf(strp, fmt, ap);
+	if (rc < 0)
+		*strp = NULL;
+	return rc;
+}
+
 __attribute__((format(printf, 2, 3)))
 int bbdd_util_fmterr(char **strp, const char *fmt, ...)
 {
 	va_list ap;
 	int rc;
 
-	if (!strp)
-		return 0;
-
 	va_start(ap, fmt);
-	rc = vasprintf(strp, fmt, ap);
+	rc = bbdd_util_vfmterr(strp, fmt, ap);
 	va_end(ap);
 
-	if (rc < 0)
-		*strp = NULL;
 	return rc;
 }
 
+__attribute__((format(printf, 2, 3)))
+int bbdd_util_wraperr(char **strp, const char *fmt, ...)
+{
+	char *new_strp = NULL;
+	va_list ap;
+	int rc;
+
+	va_start(ap, fmt);
+	rc = bbdd_util_vfmterr(&new_strp, fmt, ap);
+	va_end(ap);
+
+	if (rc >= 0) {
+		free(*strp);
+		*strp = new_strp;
+	}
+
+	return rc;
+}
 
 __attribute__((format(printf, 3, 4)))
 void bbdd_util_printerr(int rc, char **error, const char *fmt, ...)
--- bbdd-util.h
+++ bbdd-util.h
@@ -1,10 +1,24 @@
 /* SPDX-License-Identifier: BSD-3-Clause OR GPL-2.0 */
 #pragma once
+#include <stdarg.h>
 
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof(*(x)))
 
+int bbdd_util_vfmterr(char **strp, const char *fmt, va_list ap);
+
+/* Format an error into *strp. *strp is initialized to NULL on out of
+ * memory conditions, so it is in a well-defined state after the call. The
+ * incoming value of *strp can be uninitialized. */
 __attribute__((format(printf, 2, 3)))
 int bbdd_util_fmterr(char **strp, const char *fmt, ...);
 
+/* Given a valid string in *strp, form a new string, free *strp, and put
+ * the new string there. fmt can therefore reference *strp itself. Leaves
+ * *strp intact on out of memory conditions. */
+__attribute__((format(printf, 2, 3)))
+int bbdd_util_wraperr(char **strp, const char *fmt, ...);
+
+/* If rc != 0, *error shall be a valid error string, which is printed out
+ * and freed. A nop when rc == 0. */
 __attribute__((format(printf, 3, 4)))
 void bbdd_util_printerr(int rc, char **error, const char *fmt, ...);
