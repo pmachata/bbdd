07f3e2b80fea569e1cbd12562cbad94b426ab887
Petr Machata <petrm@nvidia.com>
2026-05-29 18:45:38 +0200

    tests/ns-delay: Stabilize
    The 1-s delay requires long sleeps all over the place to keeps things
    stable. Drop it to something more reasonable so that weird packets from the
    past don't throw our poll sequences et.al. off.
    
    Signed-off-by: Petr Machata <petrm@nvidia.com>

--- tests/ns-delay.sh
+++ tests/ns-delay.sh
@@ -9,10 +9,10 @@ Bbdd_connect_ns NS1 v1 $(Bbdd_IP_mask 1) \
 # Add 1-second one-way delay to each end of the link. The ping test in
 # Bbdd_connect_ns has already run without delay, so adding it here does not
 # affect the connectivity check.
-in_sockdir SD1 Bbdd_log_info "Installing netem delay 1000ms"
-in_ns NS1 Tc qdisc add dev v1 root netem delay 1000ms
+in_sockdir SD1 Bbdd_log_info "Installing netem delay 200ms"
+in_ns NS1 Tc qdisc add dev v1 root netem delay 200ms
 defer in_ns NS1 Tc qdisc del dev v1 root
-in_ns NS2 Tc qdisc add dev v2 root netem delay 1000ms
+in_ns NS2 Tc qdisc add dev v2 root netem delay 200ms
 defer in_ns NS2 Tc qdisc del dev v2 root
 
 Bbdd_setup_sockdir SD1 SD2
@@ -24,7 +24,7 @@ in_sockdir SD1 Bbdd session add dst $(Bbdd_IP 2) \
 in_sockdir SD2 Bbdd session add dst $(Bbdd_IP 1) \
 				min-tx 200ms min-rx 200ms detect-mult 6
 
-sleep 5
+sleep 2
 
 in_sockdir SD1 session_state_test up 1
 in_sockdir SD2 session_state_test up 1
@@ -63,10 +63,9 @@ tx_packets_d=$((tx_packets_1 - tx_packets_0))
 
 # With detect-mult of 6, we should see about 6 AdminDown packets before the
 # session is bpf_stable again. Due to netem, if we were to look at what gets out
-# of v1 right now, we would first see a second of state up packets, only then
-# followed by these 6 or so admin down packets. But we are checking bbdd's own
-# tx_packets counter, which shows which packets have been sent, which is what we
-# care about.
+# of v1 right now, we could first see a state up packet or few, followed by
+# these 6 admin down packets. But we are checking bbdd's own tx_packets counter,
+# which shows which packets have been sent, which is what we care about.
 ((tx_packets_d >= 6 && tx_packets_d <= 8))
 check_err $? "Expected approximately 6 packets, got $tx_packets_d"
 Bbdd_log_test "Traffic continues after shutdown for a bit"
@@ -88,7 +87,7 @@ in_sockdir SD2 session_state_test not_up 1
 
 in_sockdir SD1 Bbdd_log_info "Set admin-up"
 in_sockdir SD1 Bbdd session set no shutdown
-sleep 5
+sleep 2
 in_sockdir SD1 session_state_test up 1
 in_sockdir SD2 session_state_test up 1
 
@@ -99,16 +98,16 @@ in_sockdir SD2 session_state_test up 1
 in_sockdir SD1 Bbdd_log_info "Set admin-down & back up"
 
 in_sockdir SD1 Bbdd session set shutdown
-BBDD_SESSION_WAIT_TIME=1 in_sockdir SD1 session_state_test bpf_shutting_down 1
+in_sockdir SD1 session_state_test bpf_shutting_down 1
 in_sockdir SD1 Bbdd session set no shutdown
-BBDD_SESSION_WAIT_TIME=1 in_sockdir SD1 session_state_test bpf_shutting_down 0
-BBDD_SESSION_WAIT_TIME=5 in_sockdir SD1 session_state_test bpf_stable 1
+in_sockdir SD1 session_state_test bpf_shutting_down 0
+in_sockdir SD1 session_state_test bpf_stable 1
 
 in_sockdir SD1 Bbdd_log_info "Enter poll sequence, then set admin-down"
 
 in_sockdir SD1 Bbdd session set min-tx 199ms min-rx 199ms
-BBDD_SESSION_WAIT_TIME=1 in_sockdir SD1 session_state_test bpf_await_final 1
+in_sockdir SD1 session_state_test bpf_await_final 1
 in_sockdir SD1 Bbdd session set shutdown
-BBDD_SESSION_WAIT_TIME=3 in_sockdir SD1 session_state_test bpf_await_non_final 1
-BBDD_SESSION_WAIT_TIME=3 in_sockdir SD1 session_state_test bpf_shutting_down 1
-BBDD_SESSION_WAIT_TIME=3 in_sockdir SD1 session_state_test bpf_stable 1
+in_sockdir SD1 session_state_test bpf_await_non_final 1
+in_sockdir SD1 session_state_test bpf_shutting_down 1
+in_sockdir SD1 session_state_test bpf_stable 1
